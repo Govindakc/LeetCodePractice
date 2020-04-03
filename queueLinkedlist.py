8ad386e56539216d7318c2fa2345d82e55705f29
Govinda KC <kcgovindap@gmail.com>
2020-04-02 20:47:06 -0600

    stack and queue

---
+++ queueLinkedlist.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 10:05:40 2020
+@author: Govinda KC
+
+Implementation of Queue using Singly Linked List
+Time Complexicity: O(1) (for adding and removing the items)
+
+"""
+
+import random
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data  = data
+        self.next = next
+
+class Queue:
+    def __init__(self, head=None, tail = None):
+        self.head = head
+        self.tail = tail
+    
+    def Empty(self, L):
+        if L.head==None:
+            return True
+        
+    def Print(self, L):
+        
+        if self.Empty(L):
+            print('Linked list is Empty')
+            return
+        temp = L.head
+        count=1
+        while temp:
+            print(f"{count}--> {temp.data}")
+            temp = temp.next
+            count+=1
+    def add(self, L, item):
+        
+        newnode = Node(item)
+        if L.tail == None:
+            L.head = L.tail = newnode
+        else:
+            L.tail.next = newnode
+            L.tail = L.tail.next
+
+    def remove(self, L):
+        if L.head == None:
+            return
+        else:
+            if L.head.next == None:
+                # Last item 
+                L.head = None
+            else:
+                temp = L.head.next
+                L.head = temp
+                L.head.next = temp.next
+                   
+        
+if __name__ == '__main__':
+    
+    q = Queue() 
+    A = [2,3,5,6,7]
+    # A = [random.randint(0,100) for i in range(10)]
+    L = Queue()
+    for a in A:
+        q.add(L, a)
+        
+    # Print Linked List  
+    print("Linked List implementation of Queue")
+    q.Print(L)
+    
+    for i in range(2):
+        q.remove(L)
+    # Print Linked List
+    print('Linked List after removing items [First in First Out (FIFO)]')    
+    q.Print(L)
+
+    
+        
+        
+        
---
+++ stackLinkedlist.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 10:05:40 2020
+@author: Govinda KC
+
+Implementation of Stack using Singly Linked List
+Time Complexicity: O(1) (for adding and removing the items)
+"""
+
+import random
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data  = data
+        self.next = next
+
+class Stack:
+    def __init__(self, head=None, tail = None):
+        self.head = head
+        self.tail = tail
+        self.count = 0
+         
+    def Print(self, L):
+        temp = L.head
+        while temp: 
+            print(f"{temp.data}")
+            # print(f"{self.count}--> {temp.data}")
+            self.count -= 1
+            temp = temp.next
+            
+    def push(self, L, item):
+        
+        newnode = Node(item)
+        if L.tail == None:
+            L.head = L.tail = newnode
+            self.count += 1
+        else:
+            temp = L.head
+            L.head = newnode
+            L.head.next = temp
+            self.count += 1
+
+    def pop(self, L):
+        if L.head == None:
+            return
+        else:
+            temp = L.head.next
+            L.head = temp
+            L.head.next = temp.next
+                             
+        
+if __name__ == '__main__':
+    
+    s = Stack()
+    A = [2,3,5,6,7]
+    # A = [random.randint(0,100) for i in range(10)]
+    L = Stack()
+
+    for a in A:
+        s.push(L, a)
+    
+    print('List implementating Stack')    
+    s.Print(L)
+    
+    for i in range(2):
+        s.pop(L)
+    print('List after removing items [Last in First Out (LIFO)]')
+    s.Print(L)
+
+    
+        
+        
+        
